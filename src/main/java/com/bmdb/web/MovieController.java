24a07c9a5b90877ff1697bd3ac41b9502bb39449
Sean Blessing <snblessing@gmail.com>
2020-04-22 11:00:41 -0400

    added jsonresponse

--- src/main/java/com/bmdb/web/MovieController.java
+++ src/main/java/com/bmdb/web/MovieController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import com.bmdb.business.JsonResponse;
 import com.bmdb.business.Movie;
 import com.bmdb.db.MovieRepository;
 
@@ -16,15 +17,34 @@ public class MovieController {
 	private MovieRepository movieRepo;
 	
 	@GetMapping("/")
-	public Iterable<Movie> list() {
+	public JsonResponse list() {
+		JsonResponse jr = null;
 		List<Movie> movies = movieRepo.findAll();
-		return movies;
+		if (movies.size()==0) {
+			jr = JsonResponse.getInstance("No movies found.");
+		}
+		else {
+			jr = JsonResponse.getInstance(movies);
+		}
+		return jr;
 	}
 
 	@GetMapping("/{id}")
-	public Movie get(@PathVariable int id) {
+	public JsonResponse get(@PathVariable int id) {
+		// expected responses?
+		// 1 - a single movie
+		// 2 - bad id - no movie found
+		// 3 - Exception?? - hold off for now, implement 
+		//                   exception handling as needed
+		JsonResponse jr = null;
 		Optional<Movie> movie = movieRepo.findById(id);
-		return movie.get();
+		if (movie.isPresent()) {
+			jr = JsonResponse.getInstance(movie.get());
+		}
+		else {
+			jr = JsonResponse.getInstance("No movie found for id: "+id);
+		}
+		return jr;
 	}
 	
 	
